9d05db43e72c34697946fde87940a0d60e118386
Yevhen3 <114805048+Yevhen3@users.noreply.github.com>
2022-10-17 11:36:18 +0300

    Add files via upload

---
+++ InputOutput.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+    Console.WriteLine("Enter the number");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine($"You enter number {x}");
+}
+catch
+{
+    Console.WriteLine("You not enter the number");
+}
+
